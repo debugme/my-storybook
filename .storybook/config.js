2058bcc6937a21fe7f4a39c98ad8f6783ec7ebd5
Asad Razvi <debugme@hotmail.com>
2018-10-14 12:33:23 +0100

    Add Support For Inline Tests

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,8 @@
 import { configure } from '@storybook/react'
+import enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+enzyme.configure({ adapter: new Adapter() })
 
 const loadStories = () =>
   require('../chapters/button/story.js')
--- chapters/button/story.js
+++ chapters/button/story.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { shallow } from 'enzyme'
 import { storiesOf } from '@storybook/react'
+import { specs, describe, it } from 'storybook-addon-specifications'
+import expect from 'expect'
 import { action } from '@storybook/addon-actions'
 
 import { Button } from './component'
 
 storiesOf('Button', module)
-  .add('with caption', () =>
-    <Button caption="hello" onClick={action('clicked')} />
-  )
+  .add('with caption', () => {
+    const story = <Button caption="hello" onClick={action('clicked')} />
+
+    specs(() => describe('Button', () => {
+      it('should show passed in caption', () => {
+        const component = shallow(story)
+        expect(component.text()).toContain('hello')
+      })
+    }))
+
+    return story
+  })

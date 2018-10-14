fdee274270f337e24a279e091ae7574af5aabb92
Asad Razvi <debugme@hotmail.com>
2018-10-14 21:53:02 +0100

    Add Support For Knobs

--- chapters/button/story.js
+++ chapters/button/story.js
@@ -4,12 +4,15 @@ import { storiesOf } from '@storybook/react'
 import { specs, describe, it } from 'storybook-addon-specifications'
 import expect from 'expect'
 import { action } from '@storybook/addon-actions'
+import { withKnobs, text } from '@storybook/addon-knobs/react'
+
 
 import { Button } from './component'
 
 storiesOf('Button', module)
+  .addDecorator(withKnobs)
   .add('with caption', () => {
-    const story = <Button caption="hello" onClick={action('clicked')} />
+    const story = <Button caption={text('caption', 'hello')} onClick={action('clicked')} />
 
     specs(() => describe('Button', () => {
       it('should show passed in caption', () => {
